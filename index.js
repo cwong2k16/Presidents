b218371145a936d17c3dfb2b1696806530c91815
cwong2015 <sirhc8002@gmail.com>
2018-12-11 14:15:01 -0500

    completed main tasks. time to pretty things up and then deploy it

--- index.js
+++ index.js
@@ -18,26 +18,40 @@ router.get("/getData", (req, res) => {
     csv()
     .fromFile(csvFilePath)
     .then((jsonObj)=>{
-      presidentsObj = jsonObj;
+      if(req.params.data){
+        res.send([{"President":"A", "Birthday":1, "Death day":"...", "Death place":"ok"}]);
+      }
       res.send(jsonObj);
     });
   });
 
-router.get("/getAscending", (req, res) => {
-    return presidentsObj.sort(function(a, b) {
-      let x = a["President"]; 
-      let y = b["President"];
-      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
+  router.post("/getAscending", (req, res) => {
+      let jsonObj = req.body["data"];
+        jsonObj = jsonObj.sort(function(a, b) {
+          let x = a["President"]; 
+          let y = b["President"];
+          return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+      res.send(jsonObj);
+  });
+
+  router.post("/getDescending", (req, res) => {
+    let jsonObj = req.body["data"];
+      jsonObj = jsonObj.sort(function(a, b) {
+        let x = a["President"]; 
+        let y = b["President"];
+        return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+      });
+    res.send(jsonObj);
 });
 
-router.get("/getDescending", (req, res) => {
-  return presidentsObj.sort(function(a, b) {
-    let x = a["President"]; 
-    let y = b["President"];
-    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-  });
-});
+// router.get("/getDescending", (req, res) => {
+//   return presidentsObj.sort(function(a, b) {
+//     let x = a["President"]; 
+//     let y = b["President"];
+//     return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+//   });
+// });
 
 app.use("/api", router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
